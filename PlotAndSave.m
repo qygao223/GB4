7e8e22922b28a23016d316157ed1d4243eb6bf8b
qygao223 <77781624+qygao223@users.noreply.github.com>
2023-05-23 16:33:34 +0100

    get more plots and try to write detection

--- PlotAndSave.m
+++ PlotAndSave.m
@@ -2,10 +2,11 @@ clear
 clc
 clf
 a = arduino('COM19','Uno');
-
+global rise_flag;
+rise_flag = false;
 %change the words to change the plot title and saved file name, change
 %every time you adjust distance and concentration.
-title_words = 'Sensor Voltage - 100%, 80cm';
+title_words = 'Sensor Voltage - PWM test 15-20s series';
 
 h = animatedline;
 ax = gca;
@@ -18,7 +19,7 @@ startTime = datetime('now');
 time = 0.0;
 
 %change the run time so you can see the whole impulse response
-run_time = 30.0;
+run_time = 120.0;
 
 % the following two arrays get saved as a matlab matrix file to your
 % computer every time you run the program
@@ -35,6 +36,7 @@ while time < run_time
     t_array(end+1) = seconds(t);
     time = seconds(t);
     sensor_data(end+1) = v;
+    detection(v)
 
 end
 
@@ -45,7 +47,80 @@ end
 matrix100_30 = [t_array;sensor_data];
 save(title_words, 'matrix100_30')
 plot(t_array,sensor_data)
-ylim([0 5])
+ylim([0 3])
 f = gcf;
 title_file = append(title_words,'.png');
 exportgraphics(f,title_file,'Resolution',300)
+
+
+function result=detection(input)
+    interval = 20;
+    global rise_flag;
+    if ~rise_flag && input > 0.75
+        t = tic;
+        disp(toc(t))
+        rise_flag = true;
+    elseif input > 0.75 && rise_flag
+        if mod(floor(toc(t)),interval) == 0
+            if input > 1.33
+                % Set HIGH output
+                result="up";
+                % Replace with your code to output a HIGH signal to the Arduino
+                fprintf([result, floor(toc(t))]);
+            elseif input > 0.75 && input <= 1.1
+                % Set LOW output
+                result="down";
+                % Replace with your code to output a LOW signal to the Arduino
+                fprintf([result, floor(toc(t))]);
+            end
+        end
+    else
+        t=0;
+    end
+end
+
+
+
+function pulsemodulation(input)
+% Set the duration and time intervals
+duration = 60; % Duration in seconds
+interval = 20; % Interval in seconds
+
+% Create a timer
+t = tic;
+
+while toc(t) <= duration
+    % Check the time intervals
+    if mod(floor(toc(t)),interval) == 0
+        % First interval (0-20 sec): Input > 1.33, HIGH output
+        input = readVoltage(a,'A0');% Replace with your code to read the input value
+        
+        if input > 1.33
+            % Set HIGH output
+            % Replace with your code to output a HIGH signal to the Arduino
+            fprintf('Time: %.2f sec, Input: %.2f, Output: HIGH\n', toc(t), input);
+        end
+        
+    elseif mod(floor(toc(t)/interval), 3) == 1
+        % Second interval (20-40 sec): 0.75 < Input <= 1.33, LOW output
+        input = readVoltage(a,'A0');% Replace with your code to read the input value
+        
+        if input > 0.75 && input <= 1.33
+            % Set LOW output
+            % Replace with your code to output a LOW signal to the Arduino
+            fprintf('Time: %.2f sec, Input: %.2f, Output: LOW\n', toc(t), input);
+        end
+        
+    else
+        % Third interval (40-60 sec): Input <= 0.75, Ignore
+        input = readVoltage(a,'A0');% Replace with your code to read the input value
+        
+        if input <= 0.75
+            fprintf('Time: %.2f sec, Input: %.2f, Ignored\n', toc(t), input);
+        end
+    end
+    
+    % Add a small delay between iterations if needed
+    % pause(0.01);
+end
+end
