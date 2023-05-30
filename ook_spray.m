adad2e7d084e798b44bbd04d336a71d90084cf8b
Hexiang <hw560@cam.ac.uk>
2023-05-30 16:07:44 +0100

    [fix] spray codes update

--- ook_spray.m
+++ ook_spray.m
@@ -1,15 +1,15 @@
 clear c1;
 c1 = arduino('COM5', 'Uno');
 
-data = [1 1 1 1 1 1 1 0];
-t = 9; %sampling period
+data = [1 0 0 0 1 1 1 0];
+t = 4; %sampling period
 
 for i = 1:length(data)
     if data(i) == 1
         writeDigitalPin(c1, 'D9', 1);
         pause(0.03);
         writeDigitalPin(c1, 'D9', 0);
-        pause(0.2);
+        pause(0.8);
         writeDigitalPin(c1, 'D9', 1);
         pause(0.03);
         writeDigitalPin(c1, 'D9', 0);
--- pwm_spray.m
+++ pwm_spray.m
@@ -1,8 +1,8 @@
 clear c1;
 c1 = arduino('COM5', 'Uno');
 
-data = [1 0 1 0 1 0 1 0];
-t = 14; %sampling period
+data = [1 0 1 0 1 1 1 0];
+t = 4; %sampling period
 
 for i = 1:length(data)
     if data(i) == 0
@@ -13,18 +13,18 @@ for i = 1:length(data)
         writeDigitalPin(c1, 'D9', 1);
         pause(0.03);
         writeDigitalPin(c1, 'D9', 0);
-        pause(t);
+        pause(t+0.8);
     elseif data(i) == 1
         writeDigitalPin(c1, 'D9', 1);
         pause(0.03);
         writeDigitalPin(c1, 'D9', 0);
-        pause(0.4);
+        pause(0.8);
         writeDigitalPin(c1, 'D9', 1);
         pause(0.03);
         writeDigitalPin(c1, 'D9', 0);
         pause(t);
     else 
-        pause(t+0.46);
+        pause(t+0.86);
 
     end
 end
